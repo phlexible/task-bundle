bbedbd6514e48dae4f84dbc456752bf5f04e60b4
Stephan Wentz <swentz@brainbits.net>
2015-04-21 13:33:23 +0200

    import

---
+++ Resources/scripts/model/Comment.js
@@ -0,0 +1,9 @@
+Ext.provide('Phlexible.tasks.model.Comment');
+
+Phlexible.tasks.model.Comment = Ext.data.Record.create([
+    {name: 'id'},
+    {name: 'current_state'},
+    {name: 'comment'},
+    {name: 'create_user'},
+    {name: 'create_date'}
+]);
---
+++ Resources/scripts/model/Transition.js
@@ -0,0 +1,10 @@
+Ext.provide('Phlexible.tasks.model.Transition');
+
+Phlexible.tasks.model.Transition = Ext.data.Record.create([
+    {name: 'id'},
+    {name: 'name'},
+    {name: 'old_state'},
+    {name: 'new_state'},
+    {name: 'create_user'},
+    {name: 'create_date'}
+]);
